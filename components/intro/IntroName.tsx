f15917e438c122479abb53c20bc999912ad661a1
Srashti Chauhan <cu240250114@coeruniversity.ac.in>
2026-08-27 23:07:25 +0530

    signature animation

--- components/intro/IntroName.tsx
+++ components/intro/IntroName.tsx
@@ -1,42 +1,56 @@
-"use client";
+// "use client";
 
-import IntroButton from "./IntroButton";
+// import IntroButton from "./IntroButton";
+
+// export default function IntroName() {
+//   return (
+//     <>
+//       {/* Srashti Chauhan */}
+//       <div
+//         className="
+//           absolute
+//           left-[12%]
+//           top-[57%]
+//           -rotate-[13deg]
+//           z-30
+//           font-serif
+//           italic
+//           text-4xl
+//           md:text-5xl
+//           lg:text-6xl
+//           text-black
+//           whitespace-nowrap
+//         "
+//       >
+//         Srashti Chauhan
+//       </div>
+
+//       {/* Button */}
+//       <div
+//         className="
+//           absolute
+//           left-1/2
+//           -translate-x-1/2
+//           top-[87%]
+//           z-40
+//         "
+//       >
+//         <IntroButton />
+//       </div>
+//     </>
+//   );
+// }
+
+"use client";
 
 export default function IntroName() {
   return (
-    <>
-      {/* Srashti Chauhan */}
-      <div
-        className="
-          absolute
-          left-[12%]
-          top-[57%]
-          -rotate-[13deg]
-          z-30
-          font-serif
-          italic
-          text-4xl
-          md:text-5xl
-          lg:text-6xl
-          text-black
-          whitespace-nowrap
-        "
-      >
-        Srashti Chauhan
+    <div className="absolute left-[12%] top-[58%] z-30">
+      <div className="signature-wrapper">
+        <span className="signature-text">
+          Srashti Chauhan
+        </span>
       </div>
-
-      {/* Button */}
-      <div
-        className="
-          absolute
-          left-1/2
-          -translate-x-1/2
-          top-[87%]
-          z-40
-        "
-      >
-        <IntroButton />
-      </div>
-    </>
+    </div>
   );
 }
